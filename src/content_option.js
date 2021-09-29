8025180b248dd7c6a125a8a40c95d0772187f9a3
charan-saai <42688695+charan-saai@users.noreply.github.com>
2021-09-29 19:34:55 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,121 @@
+const logotext = "SAI CHARAN";
+const meta = {
+  title: "Sai Charan",
+  description:
+    "I’m Sai Charan data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Sai Charan",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Learning never ends and I think i am curious and enthusiastic to learn the things, can never stop this process in my life !",
+  your_img_url:
+    "https://lh3.googleusercontent.com/YawAdJdcy6Alc6LXfWA8wRHCiNs88-6358exGjXZXt49AsKzDBznd95BAhvAHLXMGDvSHrZXC3OFhuW4nTukaiCOqNaIvNRE2KYhbagmNUAWSgFb59VW0zMyBI88NEaDKZZsCWnq0Tgi2qnLLfw5SOUX2K4fq7o3DL8_WNjlrCEbbV4Pfz8hYO6GjL9bpNsQB7qHEgGNFG6uxwNJSLzpR-c_gNenVaNnBistNCzn_X73PUyOrOZUVZqFd9E_RGMeEH9Q_QgYAozcaH2ROFWcr0g17HHKBADetX5RZDc_Pnq1sHcN1uepUDZIQA8aizhr3aLldy6G6j04L67veGY8npRGIJw_1VnwR9Rya442kWL5AS8pXkUzqaRmN7pPsSr3x9Fp6yUX83LlQjmrZ87CLSegToCf8aAxpHyxxnPEDbWYvTvalOeQJQzjtrt-L-tDopuLyVLP9E7TeBmvgC0W_Lno7gM7qTZ9elaZWRD-1y1SwuNbCtj_P_MxNLxWhqJ9Y-BbiGiv399Yg00uBwJwY9sRXElC7IbL7MqQ766O2oNLgXoRRZFcA07zGiXZlMb6ExFYcayFC2DsXpGHYM1v8YWvH1-zswifmTBSaRhVmsUN4NqXaKOfXml-BLyaGRoy4HNcbfZZl66OaMd85x1V_P5KiHc8RJ5E8pV2w9qbH3IsKgJ46BnTXwenVIMwE6rd_FlyvXbS8YzQcEnA5NQUfAw=s993-no?authuser=0",
+};
+
+const dataabout = {
+  title: "about my self",
+  aboutme:
+    "Detailed-oriented, responsible, and committed software developer, with a get-it-done, on-time, and high-quality product spirit. Experience in developing, implementing, testing, and delivering complex front-end, back-end, and web applications using various programming languages and technologies. ",
+};
+const worktimeline = [
+  {
+    jobtitle: "Software Developer Intern",
+    where: "ICU medical LLP",
+    date: "2020",
+  },
+  {
+    jobtitle: "Summer Intern",
+    where: "X-Value Technologies",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Java",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Flutter",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "Web Dev",
+    description: "",
+  },
+  {
+    title: "Mobile Apps",
+    description: "",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://t3.ftcdn.net/jpg/04/22/54/18/360_F_422541881_3K2MvO9GrqJK1rLrbyADvq3Z4JmkLUpE.jpg",
+    desctiption: "Go Schon",
+    link: "https://github.com/",
+  },
+  {
+    img: "https://thumbs.dreamstime.com/b/data-mining-icon-filled-website-design-mobile-app-development-digital-money-collection-isolated-black-background-155262404.jpg",
+    desctiption:
+      "An Ensemble Random Forest Algorithm for Insurance Data Analysis",
+    link: "https://github.com/",
+  },
+  {
+    img: "https://cdn.techterms.com/img/xl/zip_162.png",
+    desctiption: "IVA.ZIP",
+    link: "https://github.com/",
+  },
+  {
+    img: "https://images-platform.99static.com//ASp0g1NhSm32agUk9JXlTphJRuQ=/831x0:1654x823/fit-in/500x500/99designs-contests-attachments/86/86401/attachment_86401056",
+    desctiption: "Cross Burrows wheeler",
+    link: "https://github.com/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "charansai781@gmail.com",
+  YOUR_FONE: "(91) 90804-63320",
+  description: "",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/charan-saai",
+  linkedin: "https://www.linkedin.com/in/sai-charan-prasad-211388169/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
